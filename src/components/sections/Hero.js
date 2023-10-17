8744f894fee27d8081b5876afd4479edd68e01c1
Laye <tlayefaah@gmail.com>
2023-10-17 14:33:44 +0100

    added floating imge

--- src/components/sections/Hero.js
+++ src/components/sections/Hero.js
@@ -1,12 +1,26 @@
 import Image from "next/image";
-import React from "react";
+import React, { useState } from "react";
 import logo from "public/$PXL8.svg";
 import x_icon from "public/icons/x-icon.svg";
 import telegram_icon from "public/icons/telegram-icon.svg";
+import globe from "public/images/globe.svg";
 import Link from "next/link";
 import DropBoxes from "../DropBoxes";
+import { MdContentCopy } from "react-icons/md";
 
 export default function Hero() {
+  const address = "0x744C26E20a231D49a23b43b1d12e6F005834a8a3";
+  const [valueToCopy, setValueToCopy] = useState(address);
+
+  const handleCopyToClipboard = async () => {
+    try {
+      await navigator.clipboard.writeText(valueToCopy);
+      alert("Value copied to clipboard!");
+    } catch (error) {
+      console.error("Error copying to clipboard:", error);
+    }
+  };
+
   return (
     <div className="h-[92vh]">
       <DropBoxes />
@@ -46,7 +60,7 @@ export default function Hero() {
               <div className="w-8 icon ">
                 <Image
                   src={telegram_icon}
-                  alt="$PXL8"
+                  alt="telegram"
                   height={500}
                   width={500}
                   priority
@@ -55,8 +69,35 @@ export default function Hero() {
               </div>
             </Link>
           </div>
+
+          <div className="mt-4">
+            <h2 className="font-semibold text-base">Contract Address</h2>
+            <div
+              onClick={handleCopyToClipboard}
+              className="flex space-x-3 w-fit p-2 border border-primary bg-[#D434FE1F] mt-4 rounded-md"
+            >
+              <div className=" text-[10px]">{address}</div>
+              <div className="">
+                <MdContentCopy />
+              </div>
+            </div>
+            <div className="mt-6">
+              <button className="btn btn-gradient ">Buy Now</button>
+            </div>
+          </div>
         </div>
-        <div className="bg-lightBlue"> image</div>
+        <div className="pt-5">
+          <div className="float">
+            <Image
+              src={globe}
+              alt="globe"
+              height={700}
+              width={700}
+              priority
+              className="h-full w-full"
+            />
+          </div>
+        </div>
       </div>
     </div>
   );
