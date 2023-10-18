e1df2c0ec63034578656b5beabf5f90115119307
Laye <tlayefaah@gmail.com>
2023-10-18 17:48:25 +0100

    tokenomics section

--- src/components/sections/Audit.js
+++ src/components/sections/Audit.js
@@ -5,7 +5,7 @@ import DropBoxes from "../DropBoxes";
 
 export default function Audit() {
   return (
-    <div className="relative min-h-[32vh] border-b border-b-[#FFFFFF20] ">
+    <div className="relative min-h-[40vh] border-b border-b-[#FFFFFF20] ">
       <DropBoxes />
       <div className="container mx-auto h-full flex justify-center items-center">
         <div className="w-full md:max-w-2xl lg:max-w-3xl px-9 py-12 md:py-20 lg:py-28">
